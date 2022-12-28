fc2a8bf7ed8f58a61a6b5f7fe398009de3619f6a
Christian Gendron <christiangendron@gmail.com>
2022-12-27 20:37:40 -0500

    feat: image proper size and resolution

--- components/PostWithImage.js
+++ components/PostWithImage.js
@@ -1,16 +1,23 @@
-import {Text, View, StyleSheet, Image} from 'react-native';
+import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import PropTypes from 'prop-types';
 import AppTheme from '../styles/AppTheme';
+import {decode} from 'html-entities';
 
 function PostWithImage(props) {
   const currPost = props.data.data;
+  const imageThumb = decode(currPost.preview.images[0].resolutions[2].url);
+  const thumbHeight = currPost.preview.images[0].resolutions[2].height;
 
   return (
     <View style={styles.container}>
       <Text>
         {currPost.title}
       </Text>
-      <Image style={styles.thumbnail} source={{uri: currPost.thumbnail}} />
+      <TouchableOpacity
+        style={{height: thumbHeight, width: '100%'}}
+        onPress={() => console.log('pressed image')}>
+        <Image style={{resizeMode: 'cover', width: '100%', height: thumbHeight}} source={{uri: imageThumb}} />
+      </TouchableOpacity>
       <Text>
         {currPost.subreddit}
       </Text>
@@ -29,13 +36,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: AppTheme.lightgray,
     flex: 1,
+    height: 'auto',
     justifyContent: 'center',
     marginVertical: 5,
-    padding: 10,
-  },
-  thumbnail: {
-    height: 400,
-    resizeMode: 'contain',
-    width: '100%',
   },
 });
