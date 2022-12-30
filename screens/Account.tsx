605138fef4e3c79be12cfb3b57ba5ca590b71743
Christian Gendron <christiangendron@gmail.com>
2022-12-29 20:46:06 -0500

    feat: placeholder text in account page

--- screens/Account.tsx
+++ screens/Account.tsx
@@ -1,11 +1,17 @@
 import {StyleSheet, View, Text} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import AppTheme from '../styles/AppTheme';
 
 export default function Account() {
 
   return (
     <View style={styles.container}>
-      <Text>Account</Text>
+      <Text>Sign in in order to access your Reddit account.</Text>
+      <TouchableOpacity
+        onPress={() => console.log('Open reddit authentification menu')}
+        style={styles.button}>
+        <Text>Sign in with Reddit</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,6 +22,15 @@ const styles = StyleSheet.create({
     backgroundColor: AppTheme.lightgray,
     flex: 1,
     justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: AppTheme.lightBlue,
+    borderRadius: 20,
+    height: 50,
+    justifyContent: 'center',
+    margin: 25,
+    width: 200,
   },
   flatlist: {
     flex: 1,
