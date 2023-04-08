0a6b5ce31dcef1ab85b92e670ba3fd557dccf3c9
Christian <christiangendron@gmail.com>
2023-04-07 21:23:00 -0400

    fix: streasmlined tailwind content module

--- screens/Account.tsx
+++ screens/Account.tsx
@@ -1,39 +1,17 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import AppTheme from '../styles/AppTheme';
 
 export default function Account() {
 
   return (
-    <View style={styles.container}>
-      <Text>Sign in in order to access your Reddit account.</Text>
+    <View className='flex flex-col flex-1 justify-center items-center bg-background'>
+      <Text className='text-sm'>Sign in in order to access your Reddit account.</Text>
       <TouchableOpacity
         onPress={() => console.log('Open reddit authentification menu')}
-        style={styles.button}>
+        className='bg-primary rounded-lg p-2 mt-5'
+        >
         <Text>Sign in with Reddit</Text>
       </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: AppTheme.lightgray,
-    flex: 1,
-    justifyContent: 'center',
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: AppTheme.lightBlue,
-    borderRadius: 20,
-    height: 50,
-    justifyContent: 'center',
-    margin: 25,
-    width: 200,
-  },
-  flatlist: {
-    flex: 1,
-    width: '100%',
-  },
-});
