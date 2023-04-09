28e7aedb8cf8fdcce6c973bc78e99f1261b97607
Christian <christiangendron@gmail.com>
2023-04-09 14:02:50 -0400

    feat: app supports videos now using expo-av

--- components/FullScreenComp.tsx
+++ components/FullScreenComp.tsx
@@ -2,13 +2,36 @@ import React from 'react';
 import { Dimensions, Image } from 'react-native';
 import { FullScreenCompProps } from '../types/FullScreenComp';
 import { decode } from 'html-entities';
+import { Video, ResizeMode } from 'expo-av';
 
 export default function FullScreenComp(props: FullScreenCompProps) {
   const currentPost = props.data.data;
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
 
-  let image = undefined;
+  let content = undefined;
 
-  if (currentPost.preview && currentPost.preview.images) {
+  if (currentPost.is_video) {
+    const videoURI = decode(currentPost.media.reddit_video.fallback_url);
+    const { width, height } = currentPost.media.reddit_video;
+
+    // Calculate the aspect ratio of the video
+    const aspectRatio = width / height;
+
+     // Calculate the height of the video based on the aspect ratio and the width of the device screen
+    const videoHeight = Dimensions.get('window').width / aspectRatio;
+
+    content = <Video
+      ref={video}
+      source={{ uri: videoURI }}
+      useNativeControls
+      shouldPlay
+      resizeMode={ResizeMode.CONTAIN}
+      style={{ width: Dimensions.get('window').width, height: videoHeight }}
+      isLooping
+      onPlaybackStatusUpdate={status => setStatus(() => status)}
+    />;
+  } else if (currentPost.preview && currentPost.preview.images) {
     const imageURI = decode(currentPost.preview.images[0].source.url);
     const { width, height } = currentPost.preview.images[0].source;
   
@@ -19,7 +42,7 @@ export default function FullScreenComp(props: FullScreenCompProps) {
     const imageHeight = Dimensions.get('window').width / aspectRatio;
   
     // Renders the image with the specified properties
-    image = <Image 
+    content = <Image 
       source={{ uri: imageURI }} 
       style={{ width: Dimensions.get('window').width, height: imageHeight, resizeMode: 'contain' }} 
     />;
@@ -27,7 +50,7 @@ export default function FullScreenComp(props: FullScreenCompProps) {
 
   return (
     <>
-      {image}
+      {content}
     </>
   );
 };
