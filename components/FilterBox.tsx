25894907f1737f661d8c224b43385ff2a2d9ef56
Christian Gendron <christiangendron@gmail.com>
2023-05-27 18:16:47 -0400

    feat: background on modal/close when pressing outside of it

--- components/FilterBox.tsx
+++ components/FilterBox.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Modal, View, Image, TouchableOpacity } from "react-native";
+import { Modal, View, Image, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
 import { FilterBoxProps } from "../types/FilterBox";
 import Option from "./items/Option";
 
@@ -22,38 +22,40 @@ export default function FilterBox(props: FilterBoxProps) {
                         setModalVisible(!modalVisible);
                     }}
                 >
-                    <View className="flex flex-1 justify-end items-center">
-                        <View className="bg-white w-full p-1 flex items-center pt-1 pb-10 rounded-lg">
-                            <Option icon={true} label="Best" filter={props.filter.current} selectable={true} handler={() => {
-                                props.filter.current = 'best'
-                                setModalVisible(!modalVisible);
-                                props.refetch();
-                            }} />
-                            <Option icon={true} label="Hot" filter={props.filter.current} selectable={true} handler={() => {
-                                props.filter.current = 'hot';
-                                setModalVisible(!modalVisible);
-                                props.refetch();
-                            }} />
-                            <Option icon={true} filter={props.filter.current} label="Controversial" selectable={true} handler={() => {
-                                props.filter.current = 'controversial'
-                                setModalVisible(!modalVisible);
-                                props.refetch();
-                            }} />
-                            <Option icon={true} label="Top" filter={props.filter.current} selectable={true} handler={() => {
-                                props.filter.current = 'top'
-                                setModalVisible(!modalVisible);
-                                props.refetch();
-                            }} />
-                            <Option icon={true} label="New" filter={props.filter.current} selectable={true} handler={() => {
-                                props.filter.current = 'new'
-                                setModalVisible(!modalVisible);
-                                props.refetch();
-                            }} />
-                            <Option label="Close" handler={() => {
-                                setModalVisible(!modalVisible);
-                            }} />
+                    <TouchableWithoutFeedback onPress={() => setModalVisible(!modalVisible)}>
+                        <View className="flex flex-1 justify-end items-center bg-black/[0.5]">
+                            <View className="bg-white w-full p-1 flex items-center pt-1 pb-10 rounded-lg">
+                                <Option icon={true} label="Best" filter={props.filter.current} selectable={true} handler={() => {
+                                    props.filter.current = 'best'
+                                    setModalVisible(!modalVisible);
+                                    props.refetch();
+                                }} />
+                                <Option icon={true} label="Hot" filter={props.filter.current} selectable={true} handler={() => {
+                                    props.filter.current = 'hot';
+                                    setModalVisible(!modalVisible);
+                                    props.refetch();
+                                }} />
+                                <Option icon={true} filter={props.filter.current} label="Controversial" selectable={true} handler={() => {
+                                    props.filter.current = 'controversial'
+                                    setModalVisible(!modalVisible);
+                                    props.refetch();
+                                }} />
+                                <Option icon={true} label="Top" filter={props.filter.current} selectable={true} handler={() => {
+                                    props.filter.current = 'top'
+                                    setModalVisible(!modalVisible);
+                                    props.refetch();
+                                }} />
+                                <Option icon={true} label="New" filter={props.filter.current} selectable={true} handler={() => {
+                                    props.filter.current = 'new'
+                                    setModalVisible(!modalVisible);
+                                    props.refetch();
+                                }} />
+                                <Option label="Close" handler={() => {
+                                    setModalVisible(!modalVisible);
+                                }} />
+                            </View>
                         </View>
-                    </View>
+                    </TouchableWithoutFeedback>
                 </Modal>
             </View>
         </>
--- components/PostOptions.tsx
+++ components/PostOptions.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Modal, View, Image, TouchableOpacity } from "react-native";
+import { Modal, View, Image, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
 import Option from "./items/Option";
 import { PostOptionsProps } from "../types/PostOptions";
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -26,30 +26,32 @@ export default function PostOptions(props: PostOptionsProps) {
                         setModalVisible(!modalVisible);
                     }}
                 >
-                    <View className="flex flex-1 justify-end items-center">
-                        <View className="bg-white w-full p-1 flex items-center pt-1 pb-10 rounded-lg">
-                            <Option label="Upvote" handler={() => {
-                                setModalVisible(!modalVisible);
-                            }} />
-                            <Option label="Downvote" handler={() => {
-                                setModalVisible(!modalVisible);
-                            }} />
-                            <Option label="Save" handler={() => {
-                                setModalVisible(!modalVisible);
-                            }} />
-                            <Option label={`r/${props.subreddit}`} handler={() => {
-                                setModalVisible(!modalVisible);
-                                navigation.push('Subreddit', { data: props.subreddit })
-                            }} />
-                            <Option label={props.author} handler={() => {
-                                setModalVisible(!modalVisible);
-                                navigation.navigate('Overview', { data: props.author })
-                            }} />
-                            <Option label="Close" handler={() => {
-                                setModalVisible(!modalVisible);
-                            }} />
+                    <TouchableWithoutFeedback onPress={() => setModalVisible(!modalVisible)}>
+                        <View className="flex flex-1 justify-end items-center bg-black/[.5]">
+                            <View className="bg-white w-full p-1 flex items-center pt-1 pb-10 rounded-lg">
+                                <Option label="Upvote" handler={() => {
+                                    setModalVisible(!modalVisible);
+                                }} />
+                                <Option label="Downvote" handler={() => {
+                                    setModalVisible(!modalVisible);
+                                }} />
+                                <Option label="Save" handler={() => {
+                                    setModalVisible(!modalVisible);
+                                }} />
+                                <Option label={`r/${props.subreddit}`} handler={() => {
+                                    setModalVisible(!modalVisible);
+                                    navigation.push('Subreddit', { data: props.subreddit })
+                                }} />
+                                <Option label={props.author} handler={() => {
+                                    setModalVisible(!modalVisible);
+                                    navigation.navigate('Overview', { data: props.author })
+                                }} />
+                                <Option label="Close" handler={() => {
+                                    setModalVisible(!modalVisible);
+                                }} />
+                            </View>
                         </View>
-                    </View>
+                    </TouchableWithoutFeedback>
                 </Modal>
             </View>
         </>
