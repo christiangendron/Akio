d0ad002c440abbfd955b2025988c5098ad8cd4bf
Christian Gendron <christiangendron@gmail.com>
2023-11-03 18:35:36 -0400

    feat: openai tests full coverage (except the function string)

---
+++ backend/tests/Feature/OpenAi/OpenAiAskTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature\OpenAi;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Http\Controllers\OpenAIController;
+
+class OpenAiAskTest extends TestCase
+{
+    public function testAskGeneratePost()
+    {
+        $response = OpenAIController::ask("Generate a post on the topic of dogs");
+
+        $response = json_decode($response);
+
+        // Assert that the response has the correct properties
+        $this->assertTrue(isset($response->title));
+        $this->assertTrue(isset($response->text_content));
+        $this->assertFalse(isset($response->description));
+    }
+
+    public function testAskGenerateCommunity()
+    {
+        $response = OpenAIController::ask("Generate a community on the topic of dogs");
+
+        $response = json_decode($response);
+
+        // Assert that the response has the correct properties
+        $this->assertTrue(isset($response->name));
+        $this->assertTrue(isset($response->description));
+        $this->assertFalse(isset($response->text_content));
+    }
+
+    public function testAskGenerateComment()
+    {
+        $response = OpenAIController::ask("Generate a comment on the topic of dogs");
+
+        $response = json_decode($response);
+
+        // Assert that the response has the correct properties
+        $this->assertFalse(isset($response->title));
+        $this->assertTrue(isset($response->text_content));
+    }
+}
